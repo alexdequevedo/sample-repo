e382a5f16f2649e81e1253b3046e290b37b5c578
srishtirathi31 <srishti_rathi@mckinsey.com>
2024-12-05 11:53:48 +0530

    feat: update logs

--- utils/DateFormatter/index.js
+++ utils/DateFormatter/index.js
@@ -16,10 +16,5 @@ function formatDate(date, format) {
   return format.replace(/YYYY|MM|DD|hh|mm|ss/g, (match) => map[match]);
 }
 
-// Example Usage
-console.log(formatDate(new Date(), "YYYY-MM-DD")); // e.g., "2024-12-04"
-console.log(formatDate(new Date(), "DD/MM/YYYY")); // e.g., "04/12/2024"
-console.log(formatDate(new Date(), "hh:mm:ss")); // e.g., "14:25:36"
-
 // Export the function for use in other modules
 module.exports = formatDate;
