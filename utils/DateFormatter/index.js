d6d2012da21c53ac6b9be7c796a498e1ce401c08
srishtirathi31 <srishti_rathi@mckinsey.com>
2024-12-05 12:57:57 +0530

    feat: remove file

--- utils/DateFormatter/index.js
+++ utils/DateFormatter/index.js
@@ -15,21 +15,3 @@ function formatDate(date, format) {
 
   return format.replace(/YYYY|MM|DD|hh|mm|ss/g, (match) => map[match]);
 }
-
-function formatDatekakakka(date, format) {
-  const d = new Date(date);
-  if (isNaN(d)) {
-    throw new Error("Invalid date provided.");
-  }
-
-  const map = {
-    YYYY: d.getFullYear(),
-    MM: String(d.getMonth() + 1).padStart(2, "0"),
-    DD: String(d.getDate()).padStart(2, "0"),
-    hh: String(d.getHours()).padStart(2, "0"),
-    mm: String(d.getMinutes()).padStart(2, "0"),
-    ss: String(d.getSeconds()).padStart(2, "0"),
-  };
-
-  return format.replace(/YYYY|MM|DD|hh|mm|ss/g, (match) => map[match]);
-}
